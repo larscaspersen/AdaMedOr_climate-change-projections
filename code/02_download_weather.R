febbc556a751a68441b287c6497aa034822d0132
larscaspersen <lars.caspersen@uni-bonn.de>
2024-11-13 14:08:42 +0100

    re-organized code folder

--- code/02_download_weather.R
+++ code/02_download_weather.R
@@ -269,6 +269,79 @@ write.csv(cieza_patched$weather, file = 'data/weather_ready/cieza_clean_patched.
 
 
 
+
+library(chillR)
+library(tidyverse)
+
+source('code/utilities/handle_gsod_new.R')
+
+adamedor <- read.csv('data/combined_phenological_data_adamedor_clean.csv') %>% 
+  filter(!(species == 'Apple' & location == 'Klein-Altendorf' & cultivar == 'Elstar' & year %in% c(2008, 2010)),
+         !(species == 'Apricot' & location == 'Cieza' & cultivar == 'Sublime' & year == 2014))
+
+unique(adamedor$location)
+
+coord_santomera<- c(-1.05, 38.06)
+
+stations <- handle_gsod_new(action = 'list_stations', time_interval = c(1990, 2020), 
+                            ocation = coord_santomera)
+
+
+# stat_spatial <- SpatialPointsDataFrame(data.frame(stations$Long, stations$Lat), stations, 
+#                                        proj4string = CRS("+init=epsg:4326"))
+# 
+# library(mapview)
+# 
+# mapview(stat_spatial)
+
+adamedor %>% 
+  filter(location == 'Santomera') %>% 
+  summarise(min(year), max(year))
+
+#download data from 1990 to 2022
+
+murcia <- handle_gsod_new(action = 'download_weather', 
+                          location = stations$chillR_code[1], 
+                          time_interval = c(1990, 2022))
+
+murcia <- handle_gsod_new(murcia)
+
+
+sum(is.na(murcia$MURCIA$Tmin))  / nrow(murcia$MURCIA)
+sum(is.na(murcia$MURCIA$Tmax))  / nrow(murcia$MURCIA)
+
+#0.4% of data missing
+
+#fill gaps with auxiliary weather stations
+
+aux1 <- handle_gsod_new(action = 'download_weather', 
+                        location = stations$chillR_code[2:4], 
+                        time_interval = c(1990, 2022)) %>% 
+  handle_gsod_new()
+
+
+patched <- chillR::patch_daily_temps(weather = murcia$MURCIA, 
+                                     patch_weather = aux1, 
+                                     vars = c('Tmin', 'Tmax'), 
+                                     max_mean_bias = 2, 
+                                     max_stdev_bias = 2,
+                                     time_interval = '2 weeks')
+
+patched$weather %>% 
+  summarise(miss_Tmin = sum(is.na(Tmin)),
+            miss_Tmax = sum(is.na(Tmax)))
+
+#5 missing in Tmin
+#18 missing in Tmax
+#after patching
+
+interpolated <- chillR::fix_weather(patched$weather)
+
+write.csv(interpolated$weather, file = 'data/weather_ready/murcia_clean.csv', 
+          row.names = FALSE)
+
+
+
 #need to load and recreate all the splits I did in the other scripts, because I forgot to save it
 
 
--- code/04_fit_almond_individual.R
+++ code/04_fit_almond_individual.R
@@ -691,3 +691,38 @@ for(cult in names(almond_fit[[1]])[14:26]){
   
 }
 
+
+
+
+
+
+library(tidyverse)
+
+#setwd('../fruittree_portfolio/')
+#load almond data
+almond_fit1 <- almond_fit2 <- almond_fit3 <- almond_fit <- list()
+
+source('code/utilities/load_save_fitting_results.R')
+
+
+for(i in 1:10){
+  
+  almond_fit1[[i]] <- load_fitting_result('data/fitting/almond/repeated_fitting_different_bounds/',
+                                          prefix = paste0('part_1_repeat', i, '_'))
+  
+  almond_fit2[[i]] <- load_fitting_result('data/fitting/almond/repeated_fitting_different_bounds/',
+                                          prefix = paste0('part_2_repeat', i, '_'))
+  
+  
+  almond_fit3[[i]] <- load_fitting_result('data/fitting/almond/repeated_fitting_different_bounds/',
+                                          prefix = paste0('part_3_repeat', i, '_'))
+  
+  almond_fit[[i]] <- c(almond_fit1[[i]], almond_fit2[[i]], almond_fit3[[i]])
+}
+
+
+#save data
+for(i in 1:10){
+  save_fitting_list(almond_fit[[i]], path = 'data/fitting/almond/repeated_fitting_new_bounds/', prefix = paste0('repeat', i, '_'))
+}
+
